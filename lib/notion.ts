201fd49f841df489f006efafb80eef60caab33ef
RSurya99 <rafli.rfsp@gmail.com>
2022-09-11 17:19:54 +0700

    feat: add notion lib

---
+++ lib/notion.ts
@@ -0,0 +1,25 @@
+import {Client} from '@notionhq/client';
+
+const client = new Client({
+  auth: process.env.NOTION_API_KEY,
+});
+
+export async function getLatestStreams() {
+  const streamDetails: any = await client.databases.query({
+    database_id: `${process.env.NOTION_DATABASE_ID}`,
+    filter: {
+      property: 'Current Stream',
+      checkbox: {
+        equals: true,
+      },
+    },
+    sorts: [
+      {
+        property: 'Date',
+        direction: 'descending',
+      },
+    ],
+    page_size: 1,
+  });
+  return streamDetails;
+}
