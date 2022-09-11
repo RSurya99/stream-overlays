f5374ef78bfaff2542085689ca859c88082ed539
RSurya99 <rafli.rfsp@gmail.com>
2022-09-11 17:33:44 +0700

    feat: add streaming layout and osu scene, add reconnecting-websocket dep, and remove unused code

--- lib/gosumemory.ts
+++ lib/gosumemory.ts
@@ -1,38 +1,33 @@
-import * as React from "react";
-import ReconnectingWebSocket from "reconnecting-websocket";
-
-// interface GosuMemory {
-//   gameplayData: GameplayData;
-//   menu: Menu;
-// }
+import * as React from "react"
+import ReconnectingWebSocket from "reconnecting-websocket"
 
 interface GameplayData {
-  [key: string]: any;
+  [key: string]: any
 }
 
 export function useGosumemory() {
-  const [gameplayData, setGameplayData] = React.useState<GameplayData>();
-  const [menu, setMenu] = React.useState<any>();
+  const [gameplayData, setGameplayData] = React.useState<GameplayData>()
+  const [menu, setMenu] = React.useState<any>()
 
   React.useEffect(() => {
-    const socket = new ReconnectingWebSocket("ws://127.0.0.1:24050/");
-    socket.onopen = () => console.log("Successfully Connected");
+    const socket = new ReconnectingWebSocket("ws://127.0.0.1:24050/")
+    socket.onopen = () => console.log("Successfully Connected")
 
     socket.onclose = (event) => {
-      // console.log("Socket Closed Connection: ", event);
-      socket.send("Client Closed!");
-    };
+      // console.log("Socket Closed Connection: ", event)
+      socket.send("Client Closed!")
+    }
 
     socket.onmessage = (event) => {
       try {
-        let data = JSON.parse(event.data);
-        setGameplayData(data.gameplay);
-        setMenu(data.menu);
+        let data = JSON.parse(event.data)
+        setGameplayData(data.gameplay)
+        setMenu(data.menu)
       } catch (err) {
-        console.log(err);
+        console.log(err)
       }
-    };
-  }, []);
+    }
+  }, [])
 
-  return [ gameplayData, menu ];
+  return [ gameplayData, menu ]
 }
