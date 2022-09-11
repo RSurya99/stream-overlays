015e9a8ce253bf798fee57f38c471dce11291fa9
RSurya99 <rafli.rfsp@gmail.com>
2022-09-11 17:30:18 +0700

    feat: add lib gosumemory

---
+++ lib/gosumemory.ts
@@ -0,0 +1,38 @@
+import * as React from "react";
+import ReconnectingWebSocket from "reconnecting-websocket";
+
+// interface GosuMemory {
+//   gameplayData: GameplayData;
+//   menu: Menu;
+// }
+
+interface GameplayData {
+  [key: string]: any;
+}
+
+export function useGosumemory() {
+  const [gameplayData, setGameplayData] = React.useState<GameplayData>();
+  const [menu, setMenu] = React.useState<any>();
+
+  React.useEffect(() => {
+    const socket = new ReconnectingWebSocket("ws://127.0.0.1:24050/");
+    socket.onopen = () => console.log("Successfully Connected");
+
+    socket.onclose = (event) => {
+      // console.log("Socket Closed Connection: ", event);
+      socket.send("Client Closed!");
+    };
+
+    socket.onmessage = (event) => {
+      try {
+        let data = JSON.parse(event.data);
+        setGameplayData(data.gameplay);
+        setMenu(data.menu);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  }, []);
+
+  return [ gameplayData, menu ];
+}
